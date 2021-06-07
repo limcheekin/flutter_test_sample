56da6dd0c4afcd5668273bbdc6aeddfd893504e6
Lim Chee Kin <limcheekin@vobject.com>
2021-06-07 16:33:26 +0800

    feat: first import of functional code of the app

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/favorite_model.dart';
+import 'screens/favorites.dart';
+import 'screens/home.dart';
+
+void main() {
+  runApp(TestingApp());
+}
+
+class TestingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<FavoriteModel>(
+      create: (context) => FavoriteModel(),
+      child: MaterialApp(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          HomePage.routeName: (context) => HomePage(),
+          FavoritesPage.routeName: (context) => FavoritesPage(),
+        },
+        initialRoute: HomePage.routeName,
+      ),
+    );
+  }
+}
